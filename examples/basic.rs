d119b5d95006f0718b57a02a0c4d129702aaf806
Hendrik Kunert <hendrik.kunert@gmail.com>
2025-01-31 20:20:42 +0100

    Add Chart

---
+++ examples/basic.rs
@@ -0,0 +1,49 @@
+extern crate pliced;
+
+use pliced::Series;
+
+use iced::{widget::container, Element, Length, Task};
+
+fn main() -> Result<(), iced::Error> {
+    iced::application(App::title, App::update, App::view).run_with(App::new)
+}
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+#[derive(Debug, Default)]
+struct App {
+    data: Vec<(f32, f32)>,
+}
+
+impl App {
+    pub fn new() -> (Self, Task<Message>) {
+        let data = (-50..=50)
+            .map(|x| x as f32 / 50.0)
+            .map(|x| (x, x * x))
+            .collect();
+
+        (Self { data }, Task::none())
+    }
+
+    pub fn title(&self) -> String {
+        "pliced".to_string()
+    }
+
+    pub fn update(&mut self, _msg: Message) -> Task<Message> {
+        Task::none()
+    }
+
+    pub fn view(&self) -> Element<'_, Message> {
+        container(
+            pliced::Chart::new()
+                .width(Length::Fill)
+                .heigth(Length::Fill)
+                .data(Series::Line {
+                    color: iced::Color::from_rgba(100.0, 150.0, 0.0, 0.8).into(),
+                    data: self.data.clone(),
+                }),
+        )
+        .into()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,7 @@ pub mod backend;
 pub mod widget;
 mod program;
 mod event;
+mod chart;
 
 pub use program::Program;
+pub use chart::{Chart, Series};
