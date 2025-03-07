564b8e1858786f71c41a4549d418ab4f4ad97863
Hendrik Kunert <hendrik.kunert@gmail.com>
2025-03-07 21:55:34 +0100

    Refactor point series into dynamic dispatch

--- examples/custom_chart.rs
+++ examples/custom_chart.rs
@@ -1,6 +1,6 @@
 extern crate pliced;
 
-use pliced::chart::{line_series, Chart, Labels, Margin};
+use pliced::chart::{line_series, point_series, Chart, Labels, Margin};
 
 use iced::{widget::container, Element, Length, Task, Theme};
 
@@ -143,20 +143,18 @@ impl App {
                     left: 0.0,
                     right: 0.0,
                 })
-                // .x_range(self.x_range.clone())
+                .x_range(self.x_range.clone())
                 .x_labels(Labels::default().format(&|v| format!("{v:.2}")))
                 .y_labels(Labels::default().format(&|v| format!("{v:.2}")))
-                //.y_range(-1.0..=1.0)
                 .push_series(line_series(self.data.iter().copied()).color(palette.primary)) // .push_series(
-                .push_series(line_series(&self.data_1).color(palette.success)), // .push_series(
-                                                                                //     line_series(self.data.iter().map(|(x, y)| (x, y * 0.5))).color(palette.success),
-                                                                                // )
-                                                                                // .push_series(
-                                                                                //     point_series(self.data.iter().map(|(x, y)| (x, y * 1.2))).color(palette.danger),
-                                                                                // )
-                                                                                // .on_press(|state| Message::MouseDown(state.get_offset()))
-                                                                                // .on_release(|state| Message::MouseUp(state.get_offset()))
-                                                                                // .on_move(|state| Message::OnMove(state.get_offset())),
+                .push_series(line_series(&self.data_1).color(palette.success))
+                .push_series(
+                    point_series::<(), _>(self.data.iter().copied().map(|(x, y)| (x, y * 1.5)))
+                        .color(palette.danger),
+                )
+                .on_press(|state| Message::MouseDown(state.get_offset()))
+                .on_release(|state| Message::MouseUp(state.get_offset()))
+                .on_move(|state| Message::OnMove(state.get_offset())),
         )
         .into()
     }
