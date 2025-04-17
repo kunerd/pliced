e9077ca1f39ad19642b9de5ba096f15fa85dc2c9
Hendrik Kunert <hendrik.kunert@gmail.com>
2025-04-17 21:06:23 +0200

    Move stucts into separate mods

--- src/chart.rs
+++ src/chart.rs
@@ -1,17 +1,24 @@
+mod axis;
 mod cartesian;
 mod items;
+mod label;
 mod series;
+mod tick;
 
-use iced::advanced::graphics::text::Paragraph;
+use axis::Axis;
 use items::Items;
+pub use label::Labels;
 pub use series::{LineSeries, line_series};
 pub use series::{PointSeries, PointStyle, point_series};
+use tick::Ticks;
 
 use core::f32;
 
 use cartesian::Plane;
+use iced::Point;
 use iced::advanced::Renderer as _;
 use iced::advanced::graphics::geometry::Renderer as _;
+use iced::advanced::graphics::text::Paragraph;
 use iced::advanced::text::Paragraph as _;
 use iced::advanced::widget::{Tree, tree};
 use iced::advanced::{Clipboard, Layout, Shell, Widget, layout, mouse, renderer};
@@ -20,7 +27,6 @@ use iced::widget::canvas::{self, Path, Stroke};
 use iced::widget::text::{LineHeight, Shaping, Wrapping};
 use iced::{Element, Length, Rectangle, Size, mouse::Cursor};
 use iced::{Font, Renderer, Vector, alignment, touch};
-use iced::{Pixels, Point};
 
 use std::marker::PhantomData;
 use std::ops::RangeInclusive;
@@ -31,8 +37,6 @@ pub struct Chart<'a, Message, Id, Theme = iced::Theme>
 where
     Message: Clone,
     Id: Clone,
-    //Data: IntoIterator + Clone,
-    //Data::Item: Into<(f32, f32)>,
 {
     width: Length,
     height: Length,
@@ -739,135 +743,6 @@ where
 {
     fn from(chart: Chart<'a, Message, Id, Theme>) -> Element<'a, Message, Theme, Renderer> {
         Element::new(chart)
-    }
-}
-
-#[derive(Clone)]
-pub enum AxisRange<T> {
-    Custom(T),
-    Automatic(Option<T>),
-}
-
-impl<T> Default for AxisRange<T> {
-    fn default() -> Self {
-        Self::Automatic(None)
-    }
-}
-
-pub struct Ticks {
-    color: iced::Color,
-    height: f32,
-    width: f32,
-    amount: usize,
-    //flip
-    //noGrid
-    //ints
-    //times
-    //limits: RangeInclusive<T>
-}
-
-impl Ticks {
-    pub fn color(mut self, color: iced::Color) -> Self {
-        self.color = color;
-        self
-    }
-
-    pub fn height(mut self, height: f32) -> Self {
-        self.height = height;
-        self
-    }
-
-    pub fn width(mut self, width: f32) -> Self {
-        self.width = width;
-        self
-    }
-
-    pub fn amount(mut self, amount: usize) -> Self {
-        self.amount = amount;
-        self
-    }
-}
-
-impl Default for Ticks {
-    fn default() -> Self {
-        Self {
-            // use color from theme
-            color: iced::Color::WHITE,
-            height: 5.0,
-            width: 1.0,
-            amount: 10,
-        }
-    }
-}
-
-pub struct Axis {
-    color: iced::Color,
-    width: f32,
-    // TODO limits
-}
-
-impl Axis {
-    pub fn color(mut self, color: iced::Color) -> Self {
-        self.color = color;
-        self
-    }
-
-    pub fn width(mut self, width: f32) -> Self {
-        self.width = width;
-        self
-    }
-}
-
-impl Default for Axis {
-    fn default() -> Self {
-        Self {
-            // use color from theme
-            color: iced::Color::WHITE,
-            width: 1.0,
-        }
-    }
-}
-
-#[derive(Default)]
-pub struct Labels<'a> {
-    color: Option<iced::Color>,
-    font_size: Option<iced::Pixels>,
-    format: Option<&'a dyn Fn(&f32) -> String>, // TODO:
-                                                // alignment
-                                                // limits
-                                                // uppercase    -- Make labels uppercase
-                                                // rotate 90    -- Rotate labels
-
-                                                // CA.alignRight   -- Anchor labels to the right
-                                                // CA.alignLeft    -- Anchor labels to the left
-
-                                                // CA.moveUp 5     -- Move 5 SVG units up
-                                                // CA.moveDown 5   -- Move 5 SVG units down
-                                                // CA.moveLeft 5   -- Move 5 SVG units left
-                                                // CA.moveRight 5  -- Move 5 SVG units right
-
-                                                // CA.amount 15   -- Change amount of ticks
-                                                // , CA.flip        -- Flip to opposite direction
-                                                // CA.withGrid    -- Add grid line by each label.
-
-                                                // CA.ints            -- Add ticks at "nice" ints
-                                                // CA.times Time.utc  -- Add ticks at "nice" times
-}
-
-impl<'a> Labels<'a> {
-    pub fn color(mut self, color: iced::Color) -> Self {
-        self.color = Some(color);
-        self
-    }
-
-    pub fn font_size(mut self, font_size: impl Into<Pixels>) -> Self {
-        self.font_size = Some(font_size.into());
-        self
-    }
-
-    pub fn format(mut self, format: &'a dyn Fn(&f32) -> String) -> Self {
-        self.format = Some(format);
-        self
     }
 }
 
